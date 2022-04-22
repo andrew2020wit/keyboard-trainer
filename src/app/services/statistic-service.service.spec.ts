1bce3e6a9806c06371dc9467b1536c0e362aca95
andrew2020wit <andrew2020wit@gmail.com>
2022-04-22 08:47:34 +0300

    unit test: StatisticService

--- src/app/services/statistic-service.service.spec.ts
+++ src/app/services/statistic-service.service.spec.ts
@@ -1,15 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { StatisticService } from './statistic-service.service';
 
-describe('GeneralServiceService', () => {
+describe('StatisticService', () => {
   let service: StatisticService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StatisticService);
+    service.takeSpeedData(500);
+    service.takeSpeedData(1500);
+    service.takeSpeedData(1000);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('presses$ should be properly computed', (done) => {
+    service.presses$.subscribe(presses => {
+      expect(presses).toBe(3);
+      done();
+    });
+  });
+
+  it('times$ should be properly computed', (done) => {
+    service.times$.subscribe(time => {
+      expect(time).toBe(3000);
+      done();
+    });
+  });
+
+  it('speedPerMinute$ should be properly computed', (done) => {
+    service.speedPerMinute$.subscribe(speedPerMinute => {
+      expect(speedPerMinute).toBe(60);
+      done();
+    });
+  });
+
 });
