35619cb2b00d856be27a5d87c88bf9fca74d5228
andrew2020wit <andrew2020wit@gmail.com>
2022-04-23 09:26:22 +0300

    test refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -21,9 +22,8 @@ describe('AppComponent', () => {
   });
 
   it('title should contain "Keyboard trainer"', () => {
-    const element: HTMLElement = TestBed.createComponent(AppComponent).nativeElement;
-    const titleSpan = element.querySelector('#title-span');
-    expect(titleSpan.textContent).toContain('Keyboard trainer');
+    const titleSpan = TestBed.createComponent(AppComponent).debugElement.query(By.css('[data-testid="title-span"]'));
+    expect(titleSpan.nativeElement.textContent).toContain('Keyboard trainer');
   });
 
 
