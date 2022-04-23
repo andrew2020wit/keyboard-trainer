abff19aa1130a8f6266a44007f36b1a713e5df3b
andrew2020wit <andrew2020wit@gmail.com>
2022-04-23 14:05:53 +0300

    test TrainerComponent refactoring

--- src/app/pages/home-page/trainer/trainer.component.spec.ts
+++ src/app/pages/home-page/trainer/trainer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {CharColors, TrainerComponent} from './trainer.component';
+import { CharColors, TrainerComponent } from './trainer.component';
+import { defaultText } from '../../../default-data/default-text';
 
 describe('TrainerComponent', () => {
   let component: TrainerComponent;
@@ -8,9 +9,8 @@ describe('TrainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TrainerComponent ]
-    })
-    .compileComponents();
+      declarations: [TrainerComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -23,34 +23,33 @@ describe('TrainerComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('inputs', () => {
-
-    component.onKeyPress({key: 'L'});
+  fit('inputs', () => {
+    component.onKeyPress({ key: defaultText[0] });
     expect(component.futureStringArr[0].char)
       .withContext('pushed first key right, check next char')
-      .toBe('o');
+      .toBe(defaultText[1]);
     expect(component.pastStringArr[0].char)
       .withContext('pushed first key right, check pushed char')
-      .toBe('L');
+      .toBe(defaultText[0]);
 
-    component.onKeyPress({key: 'a'});
+    const wrongChar = 'a';
+
+    component.onKeyPress({ key: wrongChar });
     expect(component.futureStringArr[0].char)
       .withContext('pushed second key wrong, check next char')
-      .toBe('r');
-    expect(component.pastStringArr[(component.pastStringArr.length - 1)].char)
+      .toBe(defaultText[2]);
+    expect(component.pastStringArr[component.pastStringArr.length - 1].char)
       .withContext('pushed second key wrong, check pushed char(factual)')
-      .toBe('|a|');
-    expect(component.pastStringArr[(component.pastStringArr.length - 2)].char)
+      .toBe('|' + wrongChar + '|');
+    expect(component.pastStringArr[component.pastStringArr.length - 2].char)
       .withContext('pushed second key wrong, check pushed char(required)')
-      .toBe('|o|');
+      .toBe('|' + defaultText[1] + '|');
 
     expect(component.futureStringArr[4].char)
       .withContext('pushed second key wrong, check value of penalty-char')
-      .toBe('o');
+      .toBe(defaultText[1]);
     expect(component.futureStringArr[4].color)
       .withContext('pushed second key wrong, check color of penalty-char')
       .toBe(CharColors.wrong);
-
   });
-
 });
